acbbc33924f30b3e29a4aca9e4ae5df3930eeaa2
Jehoz05 <lkjehoz@gmail.com>
2025-10-29 09:49:53 +0530

    day completed

--- coreJava/src/com/tnsif/daytwo/FirstProgram.java
+++
@@ -1,8 +0,0 @@
-//Demonstration of simple java program
-package com.tnsif.daytwo;
-
-public class FirstProgram {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-}
